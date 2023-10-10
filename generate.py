411c0bafb2488675fb60e5f67202d333d67c99f0
ClaraGard <clara.gard94@gmail.com>
2023-10-10 18:52:32 +0200

    adamW
    adamW et autres hyperparametres, k=32

--- generate.py
+++ generate.py
@@ -3,7 +3,8 @@ import numpy as np
 import os
 from tqdm import tqdm, trange
 import argparse
-
+import deepMF2
+import torch
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate a completed ratings table.')
@@ -21,10 +22,22 @@ if __name__ == '__main__':
     
 
     # Any method you want
-    average = np.nanmean(table)
-    table = np.nan_to_num(table, nan=average)
+    """average = np.nanmean(table)
+    table = np.nan_to_num(table, nan=average)"""
 
-    
+    # Replace NaN values with 0
+    table[np.isnan(table)] = 0
+    normalized_input_data = table/np.max(table)
+
+    encoded_dim = 32
+    input_size = table.shape 
+    hidden_size_row = 16
+    hidden_size_col = 64
+    model = deepMF2.ParallelLayersModel(input_size, hidden_size_row, hidden_size_col, encoded_dim)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=0.001, weight_decay=0.0001)
+    deepMF2.train_model(model, optimizer, torch.FloatTensor(normalized_input_data), num_epochs=130)
+    predicted = model(torch.FloatTensor(normalized_input_data), torch.FloatTensor(normalized_input_data).T)
+    table = predicted.detach().numpy()
 
     # Save the completed table 
     np.save("output.npy", table) ## DO NOT CHANGE THIS LINE
